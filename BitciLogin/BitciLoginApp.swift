f5c9718c6e0c346a6aea65dabfe5bdfce34ceea8
anilkaracay <anil.karacay06@hotmail.com>
2021-05-20 12:59:48 +0300

    Initial Commit

---
+++ BitciLogin/BitciLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitciLoginApp.swift
+//  BitciLogin
+//
+//  Created by Macbook Air on 20.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct BitciLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
